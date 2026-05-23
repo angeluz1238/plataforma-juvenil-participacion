ef032dcdeeaba07add1956e4f34abbb00d744058
angeluz1238 <millanangel1238@gmail.com>
2026-05-23 02:56:13 +0000

    Mejoras de diseño: categoría y botón, fondo degradado y contador separado

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const btnmensaje = document.getElementById('btnmensaje');
 const mensajeclase = document.getElementById('mensajeclase');
+const contadorClics = document.getElementById('contadorclics');
 
 let contadordeclics = 0;
 
-btnmensaje.addEventListener('click',()=> {
-  
-    mensajeclase.textContent = 'el texto que quisimos mostrar en el parrafo'+ contadordeclics;
-    contadordeclics = contadordeclics + 1;
+btnmensaje.addEventListener('click', () => {
+    mensajeclase.textContent = 'El texto que quisimos mostrar en el párrafo.';
+    contadordeclics += 1;
+    contadorClics.textContent = `Número de clics: ${contadordeclics}`;
 });
