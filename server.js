f2297696db39e4de05b2fb902778a5fd69515824
angeluz1238 <millanangel1238@gmail.com>
2026-05-27 04:18:03 +0000

    actualizacion clase 29

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 30000;
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+
+const rutacandidatos= patch.join(__dirname, 'datos', 'candidatos.json');
+
+function leerCandidatos() {
+    const data = fs.readFileSync(rutacandidatos, 'utf-8');
+    return JSON.parse(data);
+}
+
+function guardarCandidatos(candidatos) {
+    fs.writeFileSync(rutacandidatos, JSON.stringify(candidatos, null, 2));
+}
+
+app.get('/candidatos', (req, res) => {
+    const candidatos = leerCandidatos();
+    res.json(candidatos);
+});
+
+app.post('/api/candidatos', function (req, res) {
+    const nuevoCandidato = req.body;
+    id = Date.now();
+    nombre: req.body.nombre;
+    rol:req.body.rol;
+    propuesta:req.body.propuesta;
+    estado: " perfil de practica academica";    
+
+});
+
+if (nuevocandidato.nombre && nuevocandidato.rol && nuevocandidato.propuesta) {
+    return res.status(400).json({
+        mensaje: "faltan datos obligatorios"
+    });
+}
+
+const candidatos = leerCandidatos();
+candidatos.push(nuevocandidato);
+guardarCandidatos(candidatos);
+
+res.status(201).json({
+    mensaje: "candidato creado exitosamente",
+    candidato: nuevocandidato
+});
+
+app.listen(PORT,function(){
+    console.log(`Servidor funcionando en http://localhost:${PORT}`);
+}); 
